0286f8324e190a2869d81bb076f87dbfba1bf140
Anshuman bhattacharya <77188112+Anshuman0301@users.noreply.github.com>
2021-06-09 14:41:08 +0530

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,13 @@
+package JavaCodes;
+import java.lang.Math;
+public class RandomNumber
+{
+    public static void main(String args[])
+    {
+        System.out.println("1st Random Number is : " + Math.random());
+        System.out.println("2nd Random Number is : " + Math.random());
+        System.out.println("3rd Random Number is : " + Math.random());
+        System.out.println("4th Random Number is : " + Math.random());
+        System.out.println("5th Random Number is : " + Math.random());
+    }
+}
